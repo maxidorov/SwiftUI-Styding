3c4ab3b77e74de7d99027e7bd44f6ba9383f80c3
Maxim <s_maxim_v@bk.ru>
2021-07-12 01:40:37 +0300

    Chapter 1

---
+++ IntroducingSwiftUI/IntroducingSwiftUI/IntroducingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroducingSwiftUIApp.swift
+//  IntroducingSwiftUI
+//
+//  Created by Maxim V. Sidorov on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroducingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
